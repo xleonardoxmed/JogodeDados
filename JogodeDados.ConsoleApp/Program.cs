f8d7c651c7b5a28c64c0169365272b55717fcb56
leonardo medeiros <leonarodmed@hotmail.com>
2025-03-19 14:27:10 -0300

    Add project files.

---
+++ JogodeDados.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JogodeDados.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
