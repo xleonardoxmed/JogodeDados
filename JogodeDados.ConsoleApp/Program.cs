1d4aa9265b0e3f58f78d6c417d2e317af4233619
leonardo medeiros <leonarodmed@hotmail.com>
2025-03-19 14:55:52 -0300

    Versão 1

--- JogodeDados.ConsoleApp/Program.cs
+++ JogodeDados.ConsoleApp/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace JogodeDados.ConsoleApp
+﻿using System.Globalization;
+
+namespace JogodeDados.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while(true)
+            {
+                Start();
+
+                int loop = ReplayCheck();
+                if (loop != 0)
+                   break;
+            }
+        }
+        static int ReplayCheck()
+        {
+            Console.Write("Deseja continuar? (S/N): ");
+            string ToContinue = Console.ReadLine()!.ToUpper();
+            if (ToContinue != "S")
+                return 1;//sim
+            else
+                return 0;//nao
+        }
+        static int Start()
+        {
+            Console.Clear();
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("JOGO DE DADOS LAGEANO");
+            Console.WriteLine("--------------------------------------");
+
+            Console.Write("Pressione 'ENTER' para jogar os dados");
+            Console.ReadLine();
+
+            Random NumbersGenerator = new Random();
+            int result = NumbersGenerator.Next(1, 7);
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine($"O valor sorteado foi: {result}");
+            Console.WriteLine("--------------------------------------");
+            return result;
         }
     }
 }
